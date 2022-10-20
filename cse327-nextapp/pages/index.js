76747497e0b3cd016ccbf77de58903d7d6449d51
Tasfiul Hedayet <63163295+Tasfiul-Hedayet@users.noreply.github.com>
2022-10-21 05:21:16 +0600

    Update index.js
    changes in 16 no line

--- cse327-nextapp/pages/index.js
+++ cse327-nextapp/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org"> NSU_Cafe_Digitalization_System</a>
         </h1>
 
         <p className={styles.description}>
